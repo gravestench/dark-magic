df8fd0ca116a84e3360350e2aed4873d6870c7d4
Dylan Knuth <>
2023-09-17 12:12:45 -0700

    gzipping the d2 records guide files

--- pkg/services/record_manager/routes.go
+++ pkg/services/record_manager/routes.go
@@ -1,13 +1,16 @@
 package record_manager
 
 import (
+	"bytes"
+	"compress/gzip"
 	_ "embed"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-//go:embed diabloiidatafileguide.html
+//go:embed diabloiidatafileguide.html.gz
 var guide []byte
 
 func (s *Service) Slug() string {
@@ -16,7 +19,7 @@ func (s *Service) Slug() string {
 
 func (s *Service) InitRoutes(group *gin.RouterGroup) {
 	group.GET("", func(c *gin.Context) {
-		c.Data(http.StatusOK, "text/html; charset=utf-8", guide)
+		c.Data(http.StatusOK, "text/html; charset=utf-8", s.extractGzip(guide))
 	})
 
 	group.GET("Inventory", func(c *gin.Context) {
@@ -54,4 +57,21 @@ func (s *Service) InitRoutes(group *gin.RouterGroup) {
 
 func serveJSONData(c *gin.Context, data interface{}) {
 	c.JSON(http.StatusOK, data)
+}
+
+func (s *Service) extractGzip(data []byte) []byte {
+	r, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		s.logger.Fatal().Msg("ExtractTarGz: NewReader failed")
+	}
+
+	out := bytes.NewBuffer([]byte{})
+
+	// Copy the decompressed content to the output file
+	_, err = io.Copy(out, r)
+	if err != nil {
+		s.logger.Fatal().Msgf("extracting file: %v", err)
+	}
+
+	return out.Bytes()
 }
