77f5a410b1f656cb6a155a2a54da7367b4dd20ed
Dylan Knuth <dknuth0101@gmail.com>
2023-09-23 17:45:42 -0700

    prevent fatal log in pl2 loader

--- pkg/services/pl2Loader/load.go
+++ pkg/services/pl2Loader/load.go
@@ -1,6 +1,7 @@
 package pl2Loader
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/gravestench/pl2"
@@ -11,17 +12,20 @@ func (s *Service) Load(filepath string) (*pl2.PL2, error) {
 
 	stream, err := s.mpq.Load(filepath)
 	if err != nil {
-		s.logger.Fatal().Msgf("loading file %q: %v", filepath, err)
+		s.logger.Error().Msgf("loading file %q: %v", filepath, err)
+		return nil, fmt.Errorf("loading file: %v", err)
 	}
 
 	data, err := io.ReadAll(stream)
 	if err != nil {
-		s.logger.Fatal().Msgf("reading data: %v", err)
+		s.logger.Error().Msgf("reading data: %v", err)
+		return nil, fmt.Errorf("reading data: %v", err)
 	}
 
 	paletteTransform, err := pl2.FromBytes(data)
 	if err != nil {
-		s.logger.Fatal().Msgf("parsing dt1: %v", err)
+		s.logger.Error().Msgf("parsing dt1: %v", err)
+		return nil, fmt.Errorf("parsing dt1: %v", err)
 	}
 
 	return paletteTransform, nil
